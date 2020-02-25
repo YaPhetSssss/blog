21390c20fcaf2b8ec5b8afcf85cd09a40ab1d6ef
YaPhetSssss <1243958843@qq.com>
2020-02-25 16:30:52 +0800

    init application

---
+++ src/main/java/com/blog/darkvilla/DarkvillaApplication.java
@@ -0,0 +1,13 @@
+package com.blog.darkvilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkvillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkvillaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/darkvilla/DarkvillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.darkvilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkvillaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
