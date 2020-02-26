2db54d368e6df9542dc4ab9bca12b026e4fe7161
YaPhetSssss <1243958843@qq.com>
2020-02-27 03:28:57 +0800

    database

--- src/main/java/com/blog/darkvilla/Controller/AuthorizeController.java
+++ src/main/java/com/blog/darkvilla/Controller/AuthorizeController.java
@@ -2,31 +2,62 @@ package com.blog.darkvilla.Controller;
 
 import com.blog.darkvilla.DTO.AccesstokenDTO;
 import com.blog.darkvilla.DTO.GithubUser;
+import com.blog.darkvilla.Map.UserMapper;
+import com.blog.darkvilla.Model.User;
 import com.blog.darkvilla.Provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Autowired
+    private UserMapper userMapper;
+    @Value("${github.client_id}")
+    private String client_id;
+    @Value("${github.client_serect}")
+    private String client_serect;
+    @Value("${github.redict_uri}")
+    private String redict_uri;
+
     @RequestMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
+
         AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
-        accesstokenDTO.setClient_id("009145dd7fe604075289");
-        accesstokenDTO.setClient_serect("dcdad05c5387ac69af8a3b5261447dee50d745fd");
+        accesstokenDTO.setClient_id(client_id);
+        accesstokenDTO.setClient_serect(client_serect);
         accesstokenDTO.setCode(code);
-        accesstokenDTO.setRedict_uri("http://localhost:8080/callback");
+        accesstokenDTO.setRedict_uri(redict_uri);
         accesstokenDTO.setState(state);
         String token = githubProvider.getAccesstoken(accesstokenDTO);
         GithubUser githubUser = githubProvider.githubUser(token);
-        System.out.println(githubUser.getId());
-        return "index";
+        if(githubUser != null){
+            //登陆成功
+            request.getSession().setAttribute("user", githubUser);
+            User user = new User();
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setName(githubUser.getName());
+            user.setToken(UUID.randomUUID().toString());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+
+            return "redirect:/";
+        }
+        else{
+            //登陆失败
+        }
+        return "redirect:/";
     }
 
 }
---
+++ src/main/java/com/blog/darkvilla/Map/UserMapper.java
@@ -0,0 +1,12 @@
+package com.blog.darkvilla.Map;
+
+import com.blog.darkvilla.Model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into users (account_id, name, token, gmt_create, gmt_modified) values (#{accountId}, #{name}, #{token}, #{gmtCreate}, #{gmtModified})")
+    void insert(User user);
+}
